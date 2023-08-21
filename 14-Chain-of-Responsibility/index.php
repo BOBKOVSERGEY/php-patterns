0441d9da88f56016353840ad5419396777681fc0
bobkovs <aastudiya@gmail.com>
2023-08-21 15:37:29 +0300

    Chain Of Responsibility

---
+++ 14-Chain-of-Responsibility/index.php
@@ -0,0 +1,78 @@
+<?php
+namespace ChainOfResponsibility;
+use Exception;
+
+abstract class Account
+{
+    protected $successor;
+    protected float $balance;
+
+    public function setNext(Account $account): void
+    {
+        $this->successor = $account;
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function pay(float $amountToPay): void
+    {
+        if ($this->canPay($amountToPay)) {
+            echo sprintf('Paid %s using %s' . PHP_EOL, $amountToPay, get_called_class());
+        } elseif ($this->successor) {
+            echo sprintf('Cannot pay using %s. Proceeding ..' . PHP_EOL, get_called_class());
+            $this->successor->pay($amountToPay);
+        } else {
+            throw new Exception('None of the accounts have enough balance');
+        }
+    }
+
+    public function canPay($amount): bool
+    {
+        return $this->balance >= $amount;
+    }
+}
+
+class Bank extends Account
+{
+    protected float $balance;
+
+    public function __construct(float $balance)
+    {
+        $this->balance = $balance;
+    }
+}
+
+class Paypal extends Account
+{
+    protected float $balance;
+
+    public function __construct(float $balance)
+    {
+        $this->balance = $balance;
+    }
+}
+
+class Bitcoin extends Account
+{
+    protected float $balance;
+
+    public function __construct(float $balance)
+    {
+        $this->balance = $balance;
+    }
+}
+
+$bank = new Bank(100);          // Bank with balance 100
+$paypal = new Paypal(200);      // Paypal with balance 200
+$bitcoin = new Bitcoin(300);    // Bitcoin with balance 300
+
+
+$bank->setNext($paypal);
+$paypal->setNext($bitcoin);
+
+try {
+    $bank->pay(259);
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
