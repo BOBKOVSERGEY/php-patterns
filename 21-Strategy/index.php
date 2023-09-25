d5895422ff72dfd70b7bce0a4dee296de2f02ea2
bobkovs <aastudiya@gmail.com>
2023-09-25 12:19:57 +0300

    finished studying patterns

---
+++ 21-Strategy/index.php
@@ -0,0 +1,55 @@
+<?php
+namespace Strategy;
+interface SortStrategy
+{
+    public function sort(array $dataset): array;
+}
+
+class BubbleSortStrategy implements SortStrategy
+{
+    public function sort(array $dataset): array
+    {
+        echo "Sorting using bubble sort";
+
+        // Do sorting
+        return $dataset;
+    }
+}
+
+class QuickSortStrategy implements SortStrategy
+{
+    public function sort(array $dataset): array
+    {
+        echo "Sorting using quick sort";
+
+        // Do sorting
+        return $dataset;
+    }
+}
+
+class Sorter
+{
+    public function __construct(
+        protected SortStrategy $sorterSmall,
+        protected SortStrategy $sorterBig,
+    )
+    {}
+
+    public function sort(array $dataset): array
+    {
+        if (count($dataset) > 5) {
+            return $this->sorterBig->sort($dataset);
+        } else {
+            return $this->sorterSmall->sort($dataset);
+        }
+    }
+}
+
+$smallDataset = [1, 3, 4, 2];
+$bigDataset = [1, 4, 3, 2, 8, 10, 5, 6, 9, 7];
+
+$sorter = new Sorter(new BubbleSortStrategy(), new QuickSortStrategy());
+
+$sorter->sort($smallDataset); // Output : Sorting using bubble sort
+
+$sorter->sort($bigDataset); // Output : Sorting using quick sort
